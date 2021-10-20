a80bd781f4f1ecd2b38e50fbe97761e091db0fb9
Andrey <Andrey@DESKTOP-25BBB3L>
2021-10-21 01:38:46 +0300

    More delegats

---
+++ _Photoshop/Filters/TransformFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+	public class TransformFilter<TParametrs> : ParametrizedFilter<TParametrs>
+	{
+		Func<Size, TParametrs, Size> sizeTransform;
+		Func<Point, Size, TParametrs, Point?> pointTransform;
+		string name;
+		public override string ToString()
+		{
+			return name;
+		}
+
+        public TransformFilter(string name, Func<Size, TParametrs, Size> sizeTransform, Func<Point, Size, TParametrs, Point?> pointTransform)
+		{
+            this.name = name;
+            this.sizeTransform = sizeTransform;
+            this.pointTransform = pointTransform;
+		}
+        public override Photo Process(Photo original, TParametrs parameters)
+        {
+            var oldSize = new Size(original.Width, original.Height);
+            var newSize = sizeTransform(oldSize, parameters);
+            var result = new Photo(newSize.Width, newSize.Height);
+            for (int x = 0; x < newSize.Width; x++)
+                for (int y = 0; y < newSize.Height; y++)
+                {
+                    var point = new Point(x, y);
+                    var oldPoint = pointTransform(point, oldSize, parameters);
+                    if (oldPoint !=null)
+                    result[x, y] = original[oldPoint.Value.X, oldPoint.Value.Y];
+                }
+            return result;
+        }
+    }
+}
--- _Photoshop/Main.cs
+++ _Photoshop/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MyPhotoshop
@@ -25,6 +26,18 @@ namespace MyPhotoshop
 						return result;
 					}
 				));
+/*			window.AddFilter(new TransformFilter(
+					"Отразить по горизонтали",
+					size => size,
+					(point, size) => new Point(size.Width - point.X - 1,point.Y)
+
+				));
+			window.AddFilter(new TransformFilter(
+					"Поворот против чс",
+					size => new Size(size.Height,size.Width),
+					(point, size) => new Point(point.Y, point.X)
+
+				));*/
 			Application.Run (window);
 		}
 	}
